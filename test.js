48a376f3a6071f0c9c9d2e8d536140d951a05475
Subham Mani <manisubham09@gmail.com>
2023-12-16 23:48:27 +0530

    learn about global object

---
+++ test.js
@@ -0,0 +1,13 @@
+// Global object
+//console.log(global);
+
+/* setTimeout(() => {
+  clearInterval(interval);
+}, 3000);
+
+const interval = setInterval(() => {
+  console.log("Node says hello");
+}, 1000); */
+
+console.log(__dirname);
+console.log(__filename);
